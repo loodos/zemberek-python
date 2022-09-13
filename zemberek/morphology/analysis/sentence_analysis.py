0a9d92f473869d735425844fa3c693ff9396e074
harunuz <harun.uz@loodos.com>
2022-09-14 00:24:22 +0300

    made SentenceAnalysis subscriptable

--- zemberek/morphology/analysis/sentence_analysis.py
+++ zemberek/morphology/analysis/sentence_analysis.py
@@ -16,5 +16,11 @@ class SentenceAnalysis:
     def __iter__(self):
         return iter(self.word_analyses)
 
+    def __len__(self):
+        return len(self.word_analyses)
+
+    def __getitem__(self, item) -> 'SentenceWordAnalysis':
+        return self.word_analyses[item]
+
     def best_analysis(self) -> List[SingleAnalysis]:
         return [s.best_analysis for s in self.word_analyses]
