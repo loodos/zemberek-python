99513a38f71c2858c8b1cf8db28b5e4b4db0effd
harunuz <harun.uz@loodos.com>
2022-09-13 17:59:45 +0300

    updated setup.py and manifest

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -5,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='zemberek-python',
-    version='0.1.3',
+    version='0.2.0',
     author='Loodos',
     description='Python port of open source text processing library for Turkish, zemberek-nlp',
     long_description=long_description,
@@ -13,10 +14,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     package_data={
-      '': ["zemberek/resources/*.txt",
-           "zemberek/resources/*.csv",
-           "zemberek/resources/*.slm",
-           "zemberek/resources/*.pickle"]
+      '': [os.path.join("zemberek", "resources", "*.txt"),
+           os.path.join("zemberek", "resources", "*.csv"),
+           os.path.join("zemberek", "resources", "*.slm"),
+           os.path.join("zemberek", "resources", "*.pickle"),
+           os.path.join("zemberek", "resources", "ambiguity", "*")]
     },
     license='Apache License 2.0',
     url='https://www.loodos.com.tr/',
